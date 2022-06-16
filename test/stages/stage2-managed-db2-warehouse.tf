59e4bb085ed52df5116be57857de05d7691f5535
Sean Sundberg <seansund@us.ibm.com>
2022-06-16 10:02:58 -0500

    Sets managed db2 warehouse region to us-south in test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-managed-db2-warehouse.tf
+++ test/stages/stage2-managed-db2-warehouse.tf
@@ -2,6 +2,6 @@ module "db2-warehouse" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-db2-warehouse"
 
   resource_group_name = module.resource_group.name
-  resource_location   = var.region
+  resource_location   = "us-south"
   tags                = []
 }
